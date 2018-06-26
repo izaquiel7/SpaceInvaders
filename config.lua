02cca248a6363c4624e63498528f131de7b0000b
izaquiel7 <lotygun7@gmail.com>
2018-06-26 16:41:29 -0300

    PNGs e settings added

---
+++ config.lua
@@ -0,0 +1,15 @@
+--
+-- For more information on config.lua see the Project Configuration Guide at:
+-- https://docs.coronalabs.com/guide/basics/configSettings
+--
+
+application =
+{
+    content =
+    {
+      width = 768,
+      height = 1024,
+      scale = "letterbox",
+      fps = 30,
+    }
+}
