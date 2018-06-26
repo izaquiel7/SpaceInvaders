73a1cb65387cb0103e420ee966212b813202b34a
izaquiel7 <lotygun7@gmail.com>
2018-06-26 15:53:59 -0300

    added gamedados

---
+++ gamedados.lua
@@ -0,0 +1,7 @@
+M ={
+	numInvasor = 1, -- Used to keep track of the Level we are on
+	maxLevels = 2, -- Max number of Levels the game will have
+	linhaDeInvasores = 4 -- How many rows of Invaders to create
+	}
+
+return M
